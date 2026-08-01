6d6dc5658684492f001f7f73a5ba670db9f1d272
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-08-01 19:18:22 +0300

    Update Admin Page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,24 @@
+import Users from "@/components/users";
 import React from "react";
 
 const AdminPage = () => {
-  return <div>admin page</div>;
+  return (
+    <div className="flex flex-col md:flex-row gap-4 p-4">
+      {/*left side*/}
+      <div className="w-full lg:w-2/3 ">
+        {/*user cards*/}
+        <div className="flex gap-2 justify-between flex-wrap ">
+          <Users type="Student" />
+          <Users type="Teacher" />
+          <Users type="Parent" />
+          <Users type="Staff" />
+        </div>
+        <div></div>
+      </div>
+      {/*right side*/}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
 };
 
 export default AdminPage;
