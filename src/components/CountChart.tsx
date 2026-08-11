95a66483490b2817cb09844ef48cdaaa5621e53f
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-08-11 22:45:07 +0400

    add new component count chart

---
+++ src/components/CountChart.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { RadialBarChart, RadialBar, Legend, Tooltip } from "recharts";
+import Image from "next/image";
+const data = [
+  {
+    name: "Total",
+    count: 106,
+    fill: "white",
+  },
+  {
+    name: "Girls",
+    count: 53,
+    fill: "#6AC59B",
+  },
+  {
+    name: "Boys",
+    count: 53,
+    fill: "#B8D6F3",
+  },
+];
+
+// #endregion
+const style = {
+  top: "50%",
+  right: 0,
+  transform: "translate(0, -50%)",
+  lineHeight: "24px",
+};
+
+const CountChart = () => {
+  return (
+    <div className="mt-3 bg-white rounded-xl w-full h-full p-4">
+      {/*title*/}
+      <div className="flex justify-between items-center">
+        <h2>Students</h2>
+        <Image src="/moreDark.png" alt="" width={20} height={20} />
+      </div>
+      {/*CHART*/}
+      <div className="relative w-full h-[75%]">
+        <RadialBarChart
+          style={{
+            aspectRatio: 1.618,
+          }}
+          responsive
+          cx="50%"
+          cy="50%"
+          barSize={23}
+          data={data}
+        >
+          <RadialBar
+            label={{ position: "insideStart", fill: "#fff" }}
+            background
+            dataKey="count"
+          />
+        </RadialBarChart>
+        <Image
+          src="/maleFemale.png"
+          alt=""
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          width={40}
+          height={40}
+        />
+      </div>
+
+      <div className="flex justify-center gap-16 ">
+        <div className="flex flex-col gap-1 items-center">
+          <div className="w-5 h-5 bg-[#B8D6F3] rounded-full "></div>
+          <h1 className="font-bold ">1,234</h1>
+          <h2 className="text-sm">Boys (55%)</h2>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+          <div className="w-5 h-5 bg-[#6AC59B] rounded-full"></div>
+          <h1 className="font-bold">1,234</h1>
+          <h2 className="text-sm">Girls (45%)</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountChart;
